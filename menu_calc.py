06af1fbd52ef3c4bf330193d0e142fd898c5a4f1
WaleriiChirkov-16021990 <zx84058405@live.com>
2022-10-17 17:16:13 +0300

    сыро но работает

--- menu_calc.py
+++ menu_calc.py
@@ -5,6 +5,16 @@
 
 
 import re
+import math
+
+
+def check_complex(x):
+    reg = re.search(r"^[a-zA-Z]+$",r'x')
+    if reg:
+        reg = True
+    else:
+        reg = False
+    return reg
 
 
 def user_input_check(f: int, x):
@@ -26,10 +36,10 @@ def user_input_check(f: int, x):
                 if user_input.isdigit() and not isinstance(x, tuple):
                     user_input = int(user_input)
                     return user_input
-                if user_input.replace('.', '1').replace(',', '1').isdigit() and not isinstance(x, tuple):
+                elif user_input.replace('.', '1').replace(',', '1').isdigit() and not isinstance(x, tuple):
                     user_input = float(user_input)
                     return user_input
-                elif user_input.count('i') or user_input.count('j'):
+                elif check_complex(user_input):
                     ind_char = '/*+-%'
                     char_ = None
                     imaginary = None
@@ -39,14 +49,14 @@ def user_input_check(f: int, x):
                         if user_input[0] == '-':
                             sign = -1
                         if user_input[i].isalpha():
-                            imaginary = i
+                            imaginary = user_input[i]
                         for j in ind_char:
-                            if  i == j:
-                                char_ = i
+                            if  user_input[i] == j:
+                                char_ = user_input[i]
                     user_input = user_input.split(char_)
-                    user_input[0] = user_input[0].replace(' ', '')
-                    user_input[1] = user_input[1].replace(imaginary, '', 1)\
-                                    .replace(' ', '')
+                    user_input[0] = user_input[0].replace(' ', '').replace('-', '')
+                    user_input[1] = user_input[1].replace(f'{imaginary}', '', 1)\
+                        .replace(' ', '')
                     if user_input[0].isdigit() and user_input[1].isdigit() :
                         user_input = list(map(int, user_input))
                         if char_ == '-':
@@ -66,7 +76,7 @@ def user_input_check(f: int, x):
             if x != 0:
                 print('Введите второе число, подобное первому')
             else:
-                print('Введите комплексное чмсло в виде "a + bi" .')
+                print('Введите комплексное число в виде "a + bi" .')
             continue
     return user_input
 
@@ -79,16 +89,21 @@ def sum_func(x):
         nums1 = x
         nums2 = user_input_check(2, nums1)
     if isinstance(nums2, int| float) and isinstance(nums1, int| float):
-        result = nums1 + nums2
+        result = round(nums1 + nums2, 3)
         print(f'{nums1} + {nums2} = {result} ')
         return result
     else:
         result = []
         result.append(str(nums1[0] + nums2[0]))
-        result.append(str(nums1[1] + nums2[1]) + str(num2[2]) )
+        if nums1[1] + nums2[1] > 0:
+            result.append('+')
+        else:
+            result.append('-')
+        result.append(str(math.fabs((nums1[1] + nums2[1]))) + nums2[2] )
         result = ' '.join(result)
         print(f'{nums1} + {nums2} = {result} ')
         return result
+
 
 def subtraction_func(x):
     if x == 0:
@@ -99,13 +114,17 @@ def subtraction_func(x):
         nums2 = user_input_check(2, nums1)
     result = None
     if isinstance(nums2, int| float) and isinstance(nums1, int| float):
-        result = nums1 - nums2
+        result = round(nums1 - nums2, 3)
         print(f'{nums1} - {nums2} = {result} ')
         return result
     else:
         result = []
         result.append(str(nums1[0] - nums2[0]))
-        result.append(str(nums1[1] - nums2[1]) + str(num2[2]) )
+        if nums1[1] - nums2[1] > 0:
+            result.append('+')
+        else:
+            result.append('-')
+        result.append(str(math.abs(nums1[1] - nums2[1])) + nums2[2])
         result = ' '.join(result)
         print(f'{nums1} {nums2} = {result} ')
         return result
@@ -119,15 +138,17 @@ def multiplication_func(x):
         nums1 = x
         nums2 = user_input_check(2, nums1)
     if isinstance(nums2, int| float) and isinstance(nums1, int| float):
-        result = nums1 * nums2
+        result = round(nums1 * nums2, 3)
         print(f'{nums1} * {nums2} = {result} ')
         return result
     else:
         result = []
-        result.append(str(nums1[0] * nums2[0]))
-        result.append(str(nums1[0] * nums2[1]) + str(num[2]))
-        result.append(str(nums1[1] * nums2[0]) + str(num[2]))
-        result.append(str(nums1[1] + nums2[1]) + str(num2[2]))
+        result.append(str(nums1[0] * nums2[0] - nums1[1] * nums2[1]))
+        if nums1[0] * nums2[1] + nums1[1] * nums2[0] > 0:
+            result.append('+')
+        else:
+            result.append('-')
+        result.append(str(math.abs(nums1[0] * nums2[1] + nums1[1] * nums2[0])) + nums2[2])
         result = ' '.join(result)
         print(f'{nums1} * {nums2} = {result} ')
         return result
@@ -141,19 +162,21 @@ def division_func(x):
         nums1 = x
         nums2 = user_input_check(2, nums1)
     if isinstance(nums2, int| float) and isinstance(nums1, int| float):
-        result = nums1 / nums2
+        result = round(nums1 / nums2, 3)
         print(f'{nums1} / {nums2} = {result} ')
         return result
     else:
         result = []
         result.append(str((nums1[0] * nums2[0] + nums1[0] * nums2[1])/\
                             (nums2[0] ** 2 + nums2[1] ** 2)))
-        result.append(str((nums2[0] * nums1[1] - nums1[0] * nums2[1])/\
-                        (nums2[0] ** 2 + nums2[1] ** 2)) + str(num2[2]))
+        if (nums2[0] * nums1[1] - nums1[0] * nums2[1])/(nums2[0] ** 2 + nums2[1] ** 2) > 0:
+            result.append('+')
+        else:
+            result.append('-')
+        result.append(str(math.abs((nums2[0] * nums1[1] - nums1[0] * nums2[1])/(nums2[0] ** 2 + nums2[1] ** 2))) + nums2[2])
         result = ' '.join(result)
         print(f'{nums1} / {nums2} = {result} ')
         return result
-
 
 
 def menu_calc():
