55e2fcb4f942187e5d76c86f572ab104e2fc5df9
WaleriiChirkov-16021990 <zx84058405@live.com>
2022-10-23 15:37:12 +0300

    первая почти рабочая версия калькулятора v0.9

---
+++ logger_user_action.py
@@ -0,0 +1,10 @@
+# Данный блок регистрирует действия в текстовый файл.
+
+import datetime
+
+
+def logger_action(action: str):
+    today = datetime.datetime.today()
+    with open('log.txt', 'a', encoding='utf-8') as data:
+        data.write(\
+            f'{today.strftime("%Y-%m-%d-(%H) %H:%M")} Пользователь {action} \n')
